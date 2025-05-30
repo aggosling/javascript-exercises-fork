26aa2905cbe60b3cd353c69bf80c720b1e07f1a4
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-05-30 23:05:56 +0100

    JS Lessons 1-8.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeat) {
+    if (repeat < 0)
+        return 'ERROR'
+    let newStr = ''
+    for (var i = 0; i < repeat; i++) {
+        newStr += str
+    }
+    return newStr
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (var i = 0; i < args.length; i++) {
+        for (var j = 0; j < array.length; j++) {
+            var index = array.indexOf(args[i])
+            if (index > -1)
+                array.splice(index, 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
