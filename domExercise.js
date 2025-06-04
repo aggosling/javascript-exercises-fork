81c55ac312b2d59e074c6097c3179d4bf806ad74
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-06-04 20:35:53 +0100

    DOM Exercises

---
+++ domExercise.js
@@ -0,0 +1,27 @@
+const container = document.querySelector("#container");
+
+const redText = document.createElement("p");
+redText.textContent = "Hey I'm red!";
+redText.style.color = "red";
+
+container.appendChild(redText);
+
+const blueText = document.createElement("h3");
+blueText.textContent = "I'm a blue h3!";
+blueText.style.color = "blue";
+
+container.appendChild(blueText);
+
+const pinkDiv = document.createElement("div");
+pinkDiv.style["background-color"] = "pink";
+pinkDiv.style.border = "black solid 4px";
+
+container.appendChild(pinkDiv);
+
+const divHeader = document.createElement("h1");
+divHeader.textContent = "I'm in a div";
+pinkDiv.appendChild(divHeader);
+
+const divText = document.createElement("p");
+divText.textContent = "ME TOO!";
+pinkDiv.appendChild(divText);
