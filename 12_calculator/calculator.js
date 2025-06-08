05c9e5cf1b85e93339547ba0d14665fc901c5e19
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-06-08 22:04:08 +0100

    Exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length === 0)
+    return 0;
+  return array.reduce((sum, num) => sum + num);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((ans, num) => ans * num);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+  let sum = 1;
+  for (let i = 1; i <= num; i++) {
+    sum = sum * i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
