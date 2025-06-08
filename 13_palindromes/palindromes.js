347253864d16f70b2d0b6a5b37ce881b219b0b31
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-06-08 22:15:22 +0100

    Exercise 13.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let formatString = str.replaceAll(" ", "");
+    formatString = formatString.replaceAll(".","");
+    formatString = formatString.replaceAll(',',"");
+    formatString = formatString.replaceAll("!", "");
+    formatString = formatString.toLowerCase();
+    for (let i = 0; i < (formatString.length / 2); i++) {
+        if (formatString[i] !== formatString[formatString.length - 1 - i])
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
