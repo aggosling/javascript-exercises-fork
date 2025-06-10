1fc630617427dec91b83eb29f1c232df972215f6
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-06-10 20:34:31 +0100

    findTheOldest exercise.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (people) {
+    return peoplesAge = people.map(person => ({
+        name: person.name,
+        age: (person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth
+    })).sort((a, b) => b.age - a.age)[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
