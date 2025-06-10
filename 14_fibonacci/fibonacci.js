cbb32d2f20393b6367d6b365dc49b7c7268a725a
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-06-10 19:54:19 +0100

    Fibonacci exercise.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (typeof number != Number)
+        number = Number(number);
+    if (number < 0)
+        return "OOPS";
+    else if (number == 0 || number == 1)
+        return number;
+    let fibArray = [];
+    for (var i = 0; i <= number; i++) {
+        if (i == 0 || i == 1)
+            fibArray.push(1);
+        else
+            fibArray.push(fibArray[i-1] + fibArray[i-2]);
+    }
+    return fibArray[number-1];
 };
 
 // Do not edit below this line
